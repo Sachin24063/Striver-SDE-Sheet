62857ed78f5d49850804951c32422f8276491b59
SACHIN <98344845+Sachin24063@users.noreply.github.com>
2023-06-11 12:43:50 +0530

    Update Repeat and Missing Number Array.cpp

--- Day2 - Arrays Part-II/Repeat and Missing Number Array.cpp
+++ Day2 - Arrays Part-II/Repeat and Missing Number Array.cpp
@@ -1,19 +1,36 @@
 vector<int> Solution::repeatedNumber(const vector<int> &A) {
-    int n = A.size();
-    map<int,int> mp;
-    long long sum=0;
-    for(auto it: A){
-        sum+=it;
-        mp[it]++;
+    long long int n = A.size();
+    long long int xorSum = 0;
+    long long int xorNums = 0;
+    long long int xorAB = 0;
+
+    for (int i = 0; i < n; i++) {
+        xorSum ^= A[i];
+        xorNums ^= (i + 1);
     }
-    int a;
-    for(auto it:mp){    
-        if(it.second>1){
-            a = it.first;
-        }
+    xorAB = xorSum ^ xorNums;
+    long long int rightmostSetBit = xorAB & (~(xorAB - 1));
+
+    long long int num1 = 0;
+    long long int num2 = 0;
+
+    for (int i = 0; i < n; i++) {
+        if ((A[i] & rightmostSetBit) != 0)
+            num1 ^= A[i];
+        else
+            num2 ^= A[i];
     }
-    sum = sum - a;
-    long long ans = (n * (n+1)) / 2;
-       long long temp = ans-sum;
-       return {a,(int)temp};
+
+    for (int i = 1; i <= n; i++) {
+        if ((i & rightmostSetBit) != 0)
+            num1 ^= i;
+        else
+            num2 ^= i;
+    }
+    for (int i = 0; i < n; i++) {
+        if (A[i] == num1)
+            return {num1, num2};
+    }
+
+    return {num2, num1};
 }
