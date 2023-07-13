bd29283e1512521946ed8a8ffced8d47299a5f48
SACHIN <98344845+Sachin24063@users.noreply.github.com>
2023-07-13 12:35:31 +0530

    Create Repeated String Match.cpp

---
+++ Day15 - String/Repeated String Match.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int repeatedStringMatch(string a, string b) {
+     string temp = a;
+     int count = 1;
+     while(a.size()<b.size()){
+         a += temp;
+         count++;
+     }   
+    if(a.find(b) != string::npos) return count;
+    if(a.append(temp).find(b) != string::npos) return count+1;
+    return -1;
+    }
+};
